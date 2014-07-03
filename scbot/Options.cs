bea29dac6eaaa750c3c6c15f89c149b42d3a76c5
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-07-03 11:24:53 +0300

    Small refactorings

---
+++ scbot/Options.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace scbot
+{
+    public class Options
+    {
+        [VerbOption("install", HelpText = "Create a new Sitecore instance")]
+        public InstallOptions Install { get; set; }
+
+        [HelpVerbOption]
+        public string GetUsage(string verb)
+        {
+            return HelpText.AutoBuild(this, verb);
+        }
+    }
+
+    public class InstallOptions
+    {
+        [Option('c', "config", Required = true, HelpText = "Path to the config with install parameters")]
+        public string ConfigPath { get; set; }
+
+        [Option('m', "mode", DefaultValue = InstallMode.Full, HelpText = "Specify install mode: 'full', 'db' or 'client'")]
+        public InstallMode Mode { get; set; }
+    }
+
+    [Flags]
+    public enum InstallMode
+    {
+        Db = 1,
+        Client = 2,
+        Full = 3
+    }
+}
